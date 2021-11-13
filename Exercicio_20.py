e8467b4c19709404386c2b5e8007064c6d1b8c58
robeertgr <robeertgr@live.com>
2021-11-12 22:27:17 -0300

    Exercicio 20 modificado.

--- Exercicio_20.py
+++ Exercicio_20.py
@@ -2,25 +2,16 @@ a = int(input("Informe o valor A: "))
 b = int(input("Informe o valor B: "))
 c = int(input("Informe o valor C: "))
 
-while b + c < a:
+if b + c < a:
     print("O valor inserido em 'A' não pode ser maior que a soma de 'B' + 'C'")
-    a = int(input("Informe o valor A: "))
-    b = int(input("Informe o valor B: "))
-    c = int(input("Informe o valor C: "))
 
-while a + c < b:
+elif a + c < b:
     print("O valor inserido em 'B' não pode ser maior que a soma de 'A' + 'C'")
-    a = int(input("Informe o valor A: "))
-    b = int(input("Informe o valor B: "))
-    c = int(input("Informe o valor C: "))
 
-while a + b < c:
+elif a + b < c:
     print("O valor inserido em 'C' não pode ser maior que a soma de 'B' + 'A'")
-    a = int(input("Informe o valor A: "))
-    b = int(input("Informe o valor B: "))
-    c = int(input("Informe o valor C: "))
 
-if a == b == c:
+elif a == b == c:
     print("O triângulo é equilátero.")
 elif a == b != c or a == c != b or c == b != a:
     print("O triângulo é isosceles.")
